2863f4a89b8758b28074f2e40c4877319eed8108
Alex Rowley <arowley@gmail.com>
2024-12-07 20:07:42 +1100

    {feat} Solution 7 cleanup

--- 2024/07.py
+++ 2024/07.py
@@ -4,17 +4,6 @@ import aocar
 from itertools import product
 
 
-def parse_combinations(ingredients, operations):
-    rtn = ingredients.pop(0)
-    while len(ingredients) > 0:
-        operation = operations.pop(0)
-        if operation != '|':
-            rtn = rtn * ingredients.pop(0) if operation == '*' else rtn + ingredients.pop(0)
-        else:
-            rtn = int(str(rtn)+str(ingredients.pop(0)))
-    return rtn
-
-
 def parse_data(input_data):
     data = mt.Dataset({'line': [x for x in input_data.split('\n')]}) \
         .with_id('row_id').mutate_stretch(lambda line: line.split(':'), ['answer', 'ingredients']) \
@@ -23,22 +12,22 @@ def parse_data(input_data):
     return data
 
 
-def part_a(input_data):
-    data = parse_data(input_data) \
-        .mutate_stack(lambda ingredients: list(product('*+', repeat=len(ingredients) - 1)), 'operations') \
-        .replace(list, ['ingredients', 'operations']) \
-        .mutate(parse_combinations, 'evaluation') \
-        .filter(lambda answer, evaluation: answer == evaluation) \
-        .group_by(grouping_cols=['row_id'], other_cols=['answer']) \
-        .replace(lambda answer: answer[0], ['answer'])
-    return sum(data['answer'])
+def evaluate(ingredients, operations):
+    rtn = ingredients.pop(0)
+    while len(ingredients) > 0:
+        operation = operations.pop(0)
+        if operation != '|':
+            rtn = rtn * ingredients.pop(0) if operation == '*' else rtn + ingredients.pop(0)
+        else:
+            rtn = int(str(rtn) + str(ingredients.pop(0)))
+    return rtn
 
 
-def part_b(input_data):
+def solve(input_data, allowed='*+'):
     data = parse_data(input_data) \
-        .mutate_stack(lambda ingredients: list(product('*+|', repeat=len(ingredients) - 1)), 'operations') \
+        .mutate_stack(lambda ingredients: list(product(allowed, repeat=len(ingredients) - 1)), 'operations') \
         .replace(list, ['ingredients', 'operations']) \
-        .mutate(parse_combinations, 'evaluation') \
+        .mutate(evaluate, 'evaluation') \
         .filter(lambda answer, evaluation: answer == evaluation) \
         .group_by(grouping_cols=['row_id'], other_cols=['answer']) \
         .replace(lambda answer: answer[0], ['answer'])
@@ -51,14 +40,16 @@ puzzle_input_data = aocd.get_data(day=day, year=year)
 
 example_input = puzzle.examples[0].input_data
 
-part_a_example = part_a(example_input)
+part_a_example = solve(example_input)
 print(part_a_example)
 
-part_a_answer = part_a(puzzle_input_data)
+part_a_answer = solve(puzzle_input_data)
 print(part_a_answer)
 
-part_b_example = part_b(example_input)
+part_b_example = solve(example_input, allowed='*+|')
 print(part_b_example)
-#
-part_b_answer = part_b(puzzle_input_data)
-print(part_b_answer)
+
+part_b_example = solve(puzzle_input_data, allowed='*+|')
+print(part_b_example)
+
+
